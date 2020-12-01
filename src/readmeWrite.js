09f98e0baa329e8bd1fdc2d53e765ac23f5ebc86
Calterat <Calterat@gmail.com>
2020-12-01 00:20:18 -0600

    deleted uneeded directory

--- src/readmeWrite.js
+++ src/readmeWrite.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const readmeWrite = fileContent => {
-    fs.writeFile(`./dist/readme.md`, fileContent, (err) => {
+    fs.writeFile(`./dist/README.md`, fileContent, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
     });
